cf4a1fba96a131974e5794592ed523577dfb5916
Aziz Matyaqubov <a.matyaqubov1471@gmail.com>
2021-09-16 13:44:16 +0200

    Update main.py
    created get_info function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from data import data
 
-print(data[0]['name'])
+def get_info(position):
+    name = data[position]['name']
+    follower = data[position]['follower_count']
+    desc = data[position]['description']
+    country = data[position]['country']
+    return name, follower, desc, country
+
+print(get_info(1)[1])
+
+# if data[0]['follower_count'] > data[1]['follower_count']:
+#     print('Awesome')
